6f72c61ecd014edf055e9a7188b97122492bc2d6
miliberlin <michelle.liebheit@posteo.de>
2021-07-18 22:02:51 +0200

    show mobile menu on click

--- js/openMenu.js
+++ js/openMenu.js
@@ -1,5 +1,12 @@
 let menuBtn = document.getElementById("menu-btn");
+let menuItems = document.getElementById("nav-items");
 
-menuBtn.addEventListener("click", (e) => {
-  console.log(e.target.parentNode);
+menuBtn.addEventListener("click", () => {
+  if (menuBtn.classList.contains("open")) {
+    menuBtn.classList.remove("open");
+    menuItems.style.display = "none";
+  } else {
+    menuBtn.classList.add("open");
+    menuItems.style.display = "flex";
+  }
 });
